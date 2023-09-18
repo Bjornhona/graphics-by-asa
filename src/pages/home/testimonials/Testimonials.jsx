867001f096c708ba1d6944701bd902714a73a51d
Bjornhona <asaeri3@hotmail.com>
2023-09-18 17:37:11 +0200

    animation of testimonials

--- src/pages/home/testimonials/Testimonials.jsx
+++ src/pages/home/testimonials/Testimonials.jsx
@@ -13,8 +13,8 @@ const Testimonials = () => {
         </div>
         <div className="testimonial">
           <div className="testimonial__inner">
-            <p>I have worked with several different firms over time and I think Åsa is the Graphic Designer which I have the most confidence in. She has been responsive to my wishes and has always delivered a very good end product. Working with Åsa has really given me value for my money!</p>
-            <h6>Tomas Levin CEO and founder of TLW Real Estate</h6>
+            <p>Åsa is an excellent professional, with very high skills in product design, project coordination and as a problem solver. She is extremely responsible with her work. Åsa has an excellent attitude and always assumes a motivating role in meetings. I can highly recommend her!</p>
+            <h6>Enrique Frisancho Founder of Tambakunda [Innodesign Tank]</h6>
           </div>
         </div>
       </div>

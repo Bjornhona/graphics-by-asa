35c789fbe421af382415c6309d5ff71a86625074
Bjornhona <asaeri3@hotmail.com>
2023-09-19 21:23:02 +0200

    new line and margins set

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,15 +1,18 @@
 import StyledContent from '../../components/styledContent/StyledContent';
 import './contact.scss';
+import image from './designer-profile.jpg';
 
 const Contact = () => {
   return (
     <div id="contact">
-    <StyledContent title='contact'>
-      <p>Hi there,</p>
-      <p>Sounds like a great idea, tell me about it!</p>
-      <p>Send me an email to <a href={"mailto:info@graphicsbyasa.com"}>info@graphicsbyasa.com</a> or fill out the form below.</p>
-    </StyledContent>
-  </div>
+      <StyledContent title='contact'>
+        <p>Hi there,</p>
+        <p>Sounds like a great idea, tell me about it!<br />
+          Send me an email to <a href={"mailto:info@graphicsbyasa.com"}>info@graphicsbyasa.com</a> or fill out the form below.</p>
+        <img src={image} alt={"me"} />
+        <h4>Fill out the form and I´ll get back to you</h4>
+      </StyledContent>
+    </div>
   )
 }
 

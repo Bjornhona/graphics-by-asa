0b91cccaf62d14dbbe373664606b95738b827c65
Bjornhona <asaeri3@hotmail.com>
2023-11-12 20:24:07 +0100

    info and link set to item-info

--- src/pages/portfolio/portfolioItem/PortfolioItem.jsx
+++ src/pages/portfolio/portfolioItem/PortfolioItem.jsx
@@ -7,6 +7,7 @@ import withAutoplay from 'react-awesome-slider/dist/autoplay';
 import 'react-awesome-slider/dist/styles.css';
 import PortfolioContainer from '../PortfolioContainer';
 import './portfolioItem.scss';
+import linksIcon from './links-icon-1.png';
 
 const PortfolioItem = () => {
   const { id } = useParams();
@@ -20,7 +21,7 @@ const PortfolioItem = () => {
   return (
     <div id='portfolio-item'>
       <PortfolioContainer>
-          <StyledContent title={project?.data.thumb.title}>
+        <StyledContent title={project?.data.thumb.title}>
           <AutoplaySlider
             play={true}
             cancelOnInteraction={false}
@@ -30,7 +31,34 @@ const PortfolioItem = () => {
               <div key={index} data-src={image} />
             ))}
           </AutoplaySlider>
-          <p>Hello</p>
+          
+          <div className='portfolio-item-info'>
+            <div className='col'>
+              {project?.data.page.subTitle && <h4>{project?.data.page.subTitle}</h4>}
+              {project?.data.page.project && <p><strong>Project: </strong>{project?.data.page.project}</p>}
+              {project?.data.page.programLanguages && <p><strong>Program languages: </strong>{project?.data.page.programLanguages}</p>}
+              {project?.data.page.design && <p><strong>Design: </strong>{project?.data.page.design}</p>}
+              {project?.data.page.github.text && <p><strong>Github: </strong><a href={project?.data.page.github.link} target='_blank' rel="noreferrer">{project?.data.page.github.text}</a></p>}
+            </div>
+            <div className='col'>
+              {project?.data.page.text && <p>{project?.data.page.text}</p>}
+            </div>
+          </div>
+
+          <div className='skill-list'>
+            {project?.data.tags.map((skill, index) => (
+              <div key={index} className="skill-tag">
+                {skill}
+              </div>
+            ))}
+          </div>
+
+          {project?.data.page.demos.map((demo, index) => (
+            <div className={'links-item'}>
+              <img  src={linksIcon} alt='links icon' />
+              <a key={index} href={demo.link} target='_blank' rel="noreferrer">{demo.text}</a>
+            </div>
+          ))}
         </StyledContent>
       </PortfolioContainer>
     </div>
